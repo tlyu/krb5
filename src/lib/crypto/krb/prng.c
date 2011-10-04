6ad98d2fd66aeaeb77eb9b8987164649b3f424d5
Kevin Wasserman <kevin.wasserman@painless-security.com>
2011-10-04 07:26:16 -0400

    CryptAcquireContext error handling; not ready to push

--- src/lib/crypto/krb/prng.c
+++ src/lib/crypto/krb/prng.c
@@ -43,7 +43,96 @@ k5_get_os_entropy(unsigned char *buf, size_t len)
 
     if (!CryptAcquireContext(&provider, NULL, NULL, PROV_RSA_FULL,
                              CRYPT_VERIFYCONTEXT))
+    {
+        HRESULT result = GetLastError();
+        const char *msg = "Unknown Error";
+        switch (result)
+        {
+        case ERROR_BUSY: /* 107L  */
+            msg = "Some CSPs set this error if the CRYPT_DELETEKEYSET flag value is set and another thread or process is using this key container.";
+            break;
+
+        case ERROR_FILE_NOT_FOUND: /* 2L */
+            msg = "The profile of the user is not loaded and cannot be found. This happens when the application impersonates a user, for example, the IUSR_ComputerName account.";
+            break;
+
+        case ERROR_INVALID_PARAMETER: /* 87L */
+            msg = "One of the parameters contains a value that is not valid. This is most often a pointer that is not valid.";
+            break;
+
+        case ERROR_NOT_ENOUGH_MEMORY: /* 8L */
+            msg = "The operating system ran out of memory during the operation.";
+            break;
+
+        case NTE_BAD_FLAGS: /* 0x80090009L */
+            msg = "The dwFlags parameter has a value that is not valid.";
+            break;
+
+        case NTE_BAD_KEY_STATE: /* 0x8009000BL */
+            msg = "The user password has changed since the private keys were encrypted.";
+            break;
+
+        case NTE_BAD_KEYSET: /* 0x80090016L */
+            msg = "The key container could not be opened. A common cause of this error is that the key container does not exist. To create a key container, call CryptAcquireContext using the CRYPT_NEWKEYSET flag. This error code can also indicate that access to an existing key container is denied. Access rights to the container can be granted by the key set creator by using CryptSetProvParam.";
+            break;
+
+        case NTE_BAD_KEYSET_PARAM: /* 0x8009001FL */
+            msg = "The pszContainer or pszProvider parameter is set to a value that is not valid.";
+            break;
+
+        case NTE_BAD_PROV_TYPE: /* 0x80090014L */
+            msg = "The value of the dwProvType parameter is out of range. All provider types must be from 1 through 999, inclusive.";
+            break;
+
+        case NTE_BAD_SIGNATURE: /* 0x80090006L */
+            msg = "The provider DLL signature could not be verified. Either the DLL or the digital signature has been tampered with.";
+            break;
+
+        case NTE_EXISTS: /* 0x8009000FL */
+            msg = "The dwFlags parameter is CRYPT_NEWKEYSET, but the key container already exists.";
+            break;
+
+        case NTE_KEYSET_ENTRY_BAD: /* 0x8009001AL */
+            msg = "The pszContainer key container was found but is corrupt.";
+            break;
+
+        case NTE_KEYSET_NOT_DEF: /* 0x80090019L */
+            msg = "The requested provider does not exist.";
+            break;
+
+        case NTE_NO_MEMORY: /* 0x8009000EL */
+            msg = "The CSP ran out of memory during the operation.";
+            break;
+
+        case NTE_PROV_DLL_NOT_FOUND: /* 0x8009001EL */
+            msg = "The provider DLL file does not exist or is not on the current path.";
+            break;
+
+        case NTE_PROV_TYPE_ENTRY_BAD: /* 0x80090018L */
+            msg = "The provider type specified by dwProvType is corrupt. This error can relate to either the user default CSP list or the computer default CSP list.";
+            break;
+
+        case NTE_PROV_TYPE_NO_MATCH: /* 0x8009001BL */
+            msg = "The provider type specified by dwProvType does not match the provider type found. Note that this error can only occur when pszProvider specifies an actual CSP name.";
+            break;
+
+        case NTE_PROV_TYPE_NOT_DEF: /* 0x80090017L */
+            msg = "No entry exists for the provider type specified by dwProvType.";
+            break;
+
+        case NTE_PROVIDER_DLL_FAIL: /* 0x8009001DL */
+            msg = "The provider DLL file could not be loaded or failed to initialize.";
+            break;
+
+        case NTE_SIGNATURE_FILE_BAD: /* 0x8009001CL */
+            msg = "An error occurred while loading the DLL file image, prior to verifying its signature.";
+            break;
+
+        default:
+            break;
+        }
         return FALSE;
+    }
     result = CryptGenRandom(provider, len, buf);
     (void)CryptReleaseContext(provider, 0);
     return result;
